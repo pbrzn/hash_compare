89bdc422c94d8940fe6e24bc8abab08b9123f41e
Patrick Brennan <pbrennanmusic@gmail.com>
2022-07-02 02:14:33 -0400

    fix: various bug fixes and refactors, improving and/or fixing functionality of shallow and deep comparison.

--- lib/compare_hashes.rb
+++ lib/compare_hashes.rb
@@ -5,6 +5,7 @@ class CompareHashes
     @hash_one = hash_one
     @hash_two = hash_two
     @options = options
+    @results = []
   end
 
   def call
@@ -15,13 +16,12 @@ class CompareHashes
       puts "Both hashes are empty."
     end
 
-    puts "1st Hash: #{@hash_one}"
-    puts "2nd Hash: #{@hash_two}"
-
-    if !!compare_keys && !!compare_values
+    if compare_keys && compare_values && !@results.any? {|r| r == false }
       puts "Both hashes are equal!"
+      return true
     else
       puts "These hashes are not equal."
+      return false
     end
   end
 
@@ -38,11 +38,13 @@ class CompareHashes
       if one_diffs.length > 0
         puts "These are the key/value pairs from Hash One that do not appear in Hash Two:"
         one_diffs.each {|k,v| puts "#{k}: #{v}" }
+        puts "----------------------------------------------------"
       end
 
       if two_diffs.length > 0
         puts "These are the key/value pairs from Hash Two that do not appear in Hash One:"
         two_diffs.each {|k,v| puts "#{k}: #{v}" }
+        puts "----------------------------------------------------"
       end
     end
     false
@@ -50,10 +52,8 @@ class CompareHashes
 
   def compare_values
 
-    if !value_type_validator(@hash_one.values) || !value_type_validator(@hash_two.values)
-      raise TypeError.new "The following value types are acceptable: String, Boolean, Number (Integer or Float), Hash, Array."
-      puts error.message
-    end
+    self.check_values_for_errors(@hash_one.values)
+    self.check_values_for_errors(@hash_two.values)
 
     one_types = @hash_one.values.map {|v| v.class }
     two_types = @hash_two.values.map {|v| v.class }
@@ -61,15 +61,16 @@ class CompareHashes
     if @options == 'shallow' && @hash_one.values == @hash_two.values && one_types == two_types
       return true
     elsif @options == 'shallow'
-      shallow = @hash_one.keys.map {|k| self.shallow_compare({ "#{k}" => @hash_one[k] }, { "#{k}" => @hash_two[k] }) }
-      shallow.any? {|v| v == false } ? false : true
+      shallow = @hash_one.keys.each {|k| self.shallow_compare({ "#{k}" => @hash_one[k] }, { "#{k}" => @hash_two[k] }) }
     elsif @options == 'deep'
-      deep = @hash_one.keys.map {|k| self.deep_compare({ "#{k}" => @hash_one[k] }, { "#{k}" => @hash_two[k] }) }
-      deep.any? {|v| v == false } ? false : true
+      deep = @hash_one.keys.each {|k| self.deep_compare({ "#{k}" => @hash_one[k] }, { "#{k}" => @hash_two[k] }) }
     end
   end
 
   def deep_compare(element_one, element_two)
+    self.check_values_for_errors(element_one)
+    self.check_values_for_errors(element_two)
+
     key = element_one.keys.first
     one = element_one.values.first
     two = element_two.values.first
@@ -80,32 +81,69 @@ class CompareHashes
         extra = one[two.length..one.length - 1]
         puts "The following elements of the array at key #{key} of the 1st has do not appear in the array at the same key in the 2nd hash..."
         extra.each_with_index {|v, i| puts "Index: #{i}, Value: #{v}" }
+        puts "----------------------------------------------------"
+        @results << false
       elsif one.length < two.length
         extra = two[one.length..two.length - 1]
         puts "The following elements of the array at key #{key} of the 2nd has do not appear in the array at the same key in the 1st hash..."
         extra.each_with_index {|v, i| puts "Index: #{i}, Value: #{v}" }
+        puts "----------------------------------------------------"
+        @results << false
       end
 
       one.each_with_index do |v, i|
         if !!two[i]
+          self.check_values_for_errors([v, two[i]])
           if v.class == Hash && two[i].class == Hash
-            CompareHashes.new(v, two[i], 'deep').call
+            self.deep_compare(v, two[i])
           elsif v.class == Array && two[i].class == Array
-            self.compare_nested_array_elements(v, two[i], key)
+            self.deep_compare({"#{key} --> array[#{i}]" => v}, {"#{key} --> array[#{i}]" => two[i]})
+          elsif v.to_s == two[i].to_s && v.class != two[i].class
+            puts "The array elements at index #{i}, at the key \'#{key}\' in both hashes differ in type..."
+            puts "___1st Hash___"
+            puts "At #{key}: array[#{i}] is #{v}. It's type is #{v.class}"
+            puts "___2nd Hash___"
+            puts "At #{key}: array[#{i}] is #{two[i]}. It's type is #{two[i].class}"
+            puts "----------------------------------------------------"
+            @results << false
           elsif v != two[i]
             puts "The array element at index #{i}, at the key \'#{key}\' in both hashes differ..."
             puts "___1st Hash___"
             puts "At #{key}: array[#{i}] is #{v}"
             puts "___2nd Hash___"
             puts "At #{key}: array[#{i}] is #{two[i]}"
+            puts "----------------------------------------------------"
+            @results << false
           end
         end
       end
-
     elsif one.class == Hash && two.class == Hash
-      CompareHashes.new(one, two, 'deep').call
+      one.each do |k, v|
+        if !!two[k]
+          self.deep_compare({ "#{key} --> #{k}" => v }, { "#{key} --> #{k}" => two[k] })
+        end
+      end
+
+      if one.keys.length != two.keys.length
+        one_diffs = one.filter {|k,v| !two[k] }
+        two_diffs = two.filter {|k,v| !one[k] }
+
+        if one_diffs.length > 0
+          puts "These are the key/value pairs from the hash at the key #{key} from Hash One that do not appear at the same key in Hash Two:"
+          one_diffs.each {|k,v| puts "#{key} => {#{k}: #{v}}" }
+          puts "----------------------------------------------------"
+          @results << false
+        end
+
+        if two_diffs.length > 0
+          puts "These are the key/value pairs from the hash at the key #{key} from Hash Two that do not appear at the same key in Hash One:"
+          two_diffs.each {|k,v| puts "#{key} => {#{k}: #{v}}" }
+          puts "----------------------------------------------------"
+          @results << false
+        end
+      end
     else
-      self.compare_with_key(element_one, element_two, key)
+      self.compare_with_key(one, two, key)
     end
   end
 
@@ -118,70 +156,58 @@ class CompareHashes
   end
 
   def compare_with_key(one, two, key)
+    self.check_values_for_errors([one, two])
+
     if one.to_s == two.to_s && one.class != two.class
       puts "This key common to both hashes has the same values, but of two different types"
       puts "___1st Hash___"
       puts "#{key}: #{one}, where #{one}\'s type is #{one.class}"
       puts "___2nd Hash___"
       puts "#{key}: #{two}, where #{two}\'s type is #{two.class}"
-      false
+      puts "----------------------------------------------------"
+      @results << false
     elsif one != two
       puts "This key common to both hashes has differing values in each hash."
       puts "___1st Hash___"
       puts "#{key}: #{one}"
       puts "___2nd Hash___"
       puts "#{key}: #{two}"
-      false
-    else
-      true
+      puts "----------------------------------------------------"
+      @results << false
+    elsif one == two
+      @results << true
     end
   end
 
-  def compare_nested_array_elements(one, two, key)
-    if one.to_s == two.to_s && one.class != two.class
-      puts "These elements in an array nested within the hashes differ in type..."
-      puts "___From 1st Hash, nested in an array at the key \'#{key}\'___"
-      puts "#{one}, where #{one}\'s type is #{one.class}"
-      puts "___From 2nd Hash, nested in an array at the key \'#{key}\'___"
-      puts "#{two}, where #{one}\'s type is #{one.class}"
-      false
-    elsif one != two
-      puts "These elements in an array nested within the hashes differ in value..."
-      puts "___1st Hash, nested in an array at the key \'#{key}\'___"
-      puts "#{one}"
-      puts "___2nd Hash, nested in an array at the key \'#{key}\'___"
-      puts "#{two}"
-      false
-    else
-      true
-    end
-  end
-
-  def value_type_validator(values)
+  #---Value Type Checking---#
+  def check_values_for_errors(values)
     values.each do |v|
-      case v.class
-      when String
-        true
-      when Integer
-        true
-      when TrueClass
-        true
-      when FalseClass
-        true
-      when Float
-        true
-      when Hash
-        true
-      when Array
-        true
-      else
-        false
+      if value_type_validator(v) == false
+        raise TypeError.new "The following value types are acceptable: String, Boolean, Number, Hash, Array."
+        puts error.message
       end
+    end
+    false
+  end
+
+  def value_type_validator(value)
+    case value
+    when String
+      true
+    when Integer
+      true
+    when TrueClass
+      true
+    when FalseClass
+      true
+    when Float
+      true
+    when Hash
+      true
+    when Array
+      true
+    else
+      false
     end
   end
 end
-
-# a = {"a" => 1, "b" => [2,3,4,[5, 6]], "c" => 3, "d" => 4, "e" => {"f" => 12, "g" => 19}}
-# b = {"a" => 1, "b" => [2,7,4,[9, 8, 10]], "c" => 3, "d" => 4, "e" => {"f" => 12, "g" => 20}}
-#
-# CompareHashes.new(a, b, 'deep').call
